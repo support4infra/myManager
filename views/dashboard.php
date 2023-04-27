054e817b5e17dcb28b620e561f68508f098b812a
support4infra <suporte@4infra.com.br>
2023-04-27 08:49:18 -0300

    ajustes na tela de dashboard

--- views/dashboard.php
+++ views/dashboard.php
@@ -4,8 +4,8 @@
             <div class="card-block">
                 <div class="row align-items-center">
                     <div class="col-8">
-                        <h4 class="text-c-yellow f-w-600">$30200</h4>
-                        <h6 class="text-muted m-b-0">All Earnings</h6>
+                        <h4 class="text-c-yellow f-w-600">30.200</h4>
+                        <h6 class="text-muted m-b-0">Licenças</h6>
                     </div>
                     <div class="col-4 text-right">
                         <i class="fa fa-barcode f-28"></i>
@@ -15,7 +15,7 @@
             <div class="card-footer bg-c-yellow">
                 <div class="row align-items-center">
                     <div class="col-9">
-                        <p class="text-white m-b-0">Licenças</p>
+                        <p class="text-white m-b-0"><a href="<?php echo BASE_URL;?>licencas" class="text-white m-b-0"> Acessar </a></p>
                     </div>
                 </div>
             </div>
@@ -26,8 +26,8 @@
             <div class="card-block">
                 <div class="row align-items-center">
                     <div class="col-8">
-                        <h4 class="text-c-green f-w-600">290+</h4>
-                        <h6 class="text-muted m-b-0">Page Views</h6>
+                        <h4 class="text-c-green f-w-600">290</h4>
+                        <h6 class="text-muted m-b-0">Clientes</h6>
                     </div>
                     <div class="col-4 text-right">
                         <i class="fa fa-building-o f-28"></i>
@@ -37,7 +37,7 @@
             <div class="card-footer bg-c-green">
                 <div class="row align-items-center">
                     <div class="col-9">
-                        <p class="text-white m-b-0">Clientes</p>
+                        <p class="text-white m-b-0"><a href="<?php echo BASE_URL;?>clientes" class="text-white m-b-0"> Acessar </a></p>
                     </div>
                 </div>
             </div>
@@ -49,7 +49,7 @@
                 <div class="row align-items-center">
                     <div class="col-8">
                         <h4 class="text-c-pink f-w-600">145</h4>
-                        <h6 class="text-muted m-b-0">Task Completed</h6>
+                        <h6 class="text-muted m-b-0">Licenças Vencendo</h6>
                     </div>
                     <div class="col-4 text-right">
                         <i class="feather icon-calendar f-28"></i>
@@ -59,7 +59,7 @@
             <div class="card-footer bg-c-pink">
                 <div class="row align-items-center">
                     <div class="col-9">
-                        <p class="text-white m-b-0">Licenças Vencendo</p>
+                        <p class="text-white m-b-0"><a href="<?php echo BASE_URL;?>licencas" class="text-white m-b-0"> Acessar </a></p>
                     </div>
                 </div>
             </div>
@@ -71,7 +71,7 @@
                 <div class="row align-items-center">
                     <div class="col-8">
                         <h4 class="text-c-blue f-w-600">500</h4>
-                        <h6 class="text-muted m-b-0">Downloads</h6>
+                        <h6 class="text-muted m-b-0">Usuários</h6>
                     </div>
                     <div class="col-4 text-right">
                         <i class="feather icon-users f-28"></i>
@@ -81,7 +81,7 @@
             <div class="card-footer bg-c-blue">
                 <div class="row align-items-center">
                     <div class="col-9">
-                        <p class="text-white m-b-0">Usuários</p>
+                        <p class="text-white m-b-0"><a href="<?php echo BASE_URL;?>usuarios" class="text-white m-b-0"> Acessar </a></p>
                     </div>
                     <div class="col-3 text-right">
                         <i class="feather icon-trending-up text-white f-16"></i>
@@ -90,9 +90,7 @@
             </div>
         </div>
     </div>
-                                            <!-- task, page, download counter  end -->
 
-                                            <!-- visitor start -->
                                             <div class="col-xl-8 col-md-12">
                                                 <div class="card">
                                                     <div class="card-header">

f2c8a199bf68c25b5c2663d7f1c91328e36dc09f
support4infra <suporte@4infra.com.br>
2023-04-28 22:31:51 -0300

    Personalização tela de login

--- views/template.php
+++ views/template.php
@@ -1,66 +1,63 @@
 <!DOCTYPE html>
 <html lang="pt-br">
-<head>
-    <title>4¡nfra - MyManager</title>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0, minimal-ui">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="description" content="#">
-    <meta name="keywords" content="flat ui, admin Admin , Responsive, Landing, Bootstrap, App, Template, Mobile, iOS, Android, apple, creative app">
-    <meta name="author" content="#">
-    <link rel="icon" href="..\assets\images\favicon.ico" type="image/x-icon">
-    <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,600,800" rel="stylesheet">
-    <link rel="stylesheet" type="text/css" href="..\assets\bower_components\bootstrap\css\bootstrap.min.css">
-    <link rel="stylesheet" type="text/css" href="..\assets\icon\feather\css\feather.css">
-    <link rel="stylesheet" type="text/css" href="..\assets\icon\font-awesome\css\font-awesome.min.css">
-    <link rel="stylesheet" type="text/css" href="..\assets\css\style.css">
-    <link rel="stylesheet" type="text/css" href="..\assets\css\jquery.mCustomScrollbar.css">
-</head>
-
-<body>
-    <div class="theme-loader">
-        <div class="ball-scale">
-            <div class='contain'>
-                <div class="ring">
-                    <div class="frame"></div>
-                </div>
-                <div class="ring">
-                    <div class="frame"></div>
-                </div>
-                <div class="ring">
-                    <div class="frame"></div>
-                </div>
-                <div class="ring">
-                    <div class="frame"></div>
-                </div>
-                <div class="ring">
-                    <div class="frame"></div>
-                </div>
-                <div class="ring">
-                    <div class="frame"></div>
-                </div>
-                <div class="ring">
-                    <div class="frame"></div>
-                </div>
-                <div class="ring">
-                    <div class="frame"></div>
-                </div>
-                <div class="ring">
-                    <div class="frame"></div>
-                </div>
-                <div class="ring">
-                    <div class="frame"></div>
+    <head>
+        <title>4¡nfra - MyManager</title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0, minimal-ui">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="description" content="#">
+        <meta name="keywords" content="flat ui, admin Admin , Responsive, Landing, Bootstrap, App, Template, Mobile, iOS, Android, apple, creative app">
+        <meta name="author" content="#">
+        <link rel="icon" href="..\assets\images\favicon.ico" type="image/x-icon">
+        <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,600,800" rel="stylesheet">
+        <link rel="stylesheet" type="text/css" href="..\assets\bower_components\bootstrap\css\bootstrap.min.css">
+        <link rel="stylesheet" type="text/css" href="..\assets\icon\feather\css\feather.css">
+        <link rel="stylesheet" type="text/css" href="..\assets\icon\font-awesome\css\font-awesome.min.css">
+        <link rel="stylesheet" type="text/css" href="..\assets\css\style.css">
+        <link rel="stylesheet" type="text/css" href="..\assets\css\jquery.mCustomScrollbar.css">
+    </head>
+    <body>
+        <div class="theme-loader">
+            <div class="ball-scale">
+                <div class='contain'>
+                    <div class="ring">
+                        <div class="frame"></div>
+                    </div>
+                    <div class="ring">
+                        <div class="frame"></div>
+                    </div>
+                    <div class="ring">
+                        <div class="frame"></div>
+                    </div>
+                    <div class="ring">
+                        <div class="frame"></div>
+                    </div>
+                    <div class="ring">
+                        <div class="frame"></div>
+                    </div>
+                    <div class="ring">
+                        <div class="frame"></div>
+                    </div>
+                    <div class="ring">
+                        <div class="frame"></div>
+                    </div>
+                    <div class="ring">
+                        <div class="frame"></div>
+                    </div>
+                    <div class="ring">
+                        <div class="frame"></div>
+                    </div>
+                    <div class="ring">
+                        <div class="frame"></div>
+                    </div>
                 </div>
             </div>
-        </div>
-    </div>    
-    <div id="pcoded" class="pcoded">
-        <div class="pcoded-overlay-box"></div>
-        <div class="pcoded-container navbar-wrapper">
-            <?php include 'templates/topo.php' ;?>    
-
-            
-            <div class="showChat_inner">
+        </div>    
+        <div id="pcoded" class="pcoded">
+            <div class="pcoded-overlay-box"></div>
+            <div class="pcoded-container navbar-wrapper">
+                <?php include 'templates/topo.php' ;?>              
+                <div class="showChat_inner">
                 <div class="media chat-inner-header">
                     <a class="back_chatBox">
                         <i class="feather icon-chevron-left"></i> Josephin Doe
@@ -118,32 +115,29 @@
                 </div>
             </div>
         </div>
-    </div>
-    <script type="text/javascript" src="..\assets\bower_components\jquery\js\jquery.min.js"></script>
-    <script type="text/javascript" src="..\assets\bower_components\jquery-ui\js\jquery-ui.min.js"></script>
-    <script type="text/javascript" src="..\assets\bower_components\popper.js\js\popper.min.js"></script>
-    <script type="text/javascript" src="..\assets\bower_components\bootstrap\js\bootstrap.min.js"></script>
-    <script type="text/javascript" src="..\assets\bower_components\jquery-slimscroll\js\jquery.slimscroll.js"></script>
-    <script type="text/javascript" src="..\assets\bower_components\modernizr\js\modernizr.js"></script>
-    <script type="text/javascript" src="..\assets\bower_components\modernizr\js\css-scrollbars.js"></script>
-    <script type="text/javascript" src="..\assets\bower_components\chart.js\js\Chart.js"></script>
-    <script src="..\assets\pages\widget\amchart\amcharts.js"></script>
-    <script src="..\assets\pages\widget\amchart\serial.js"></script>
-    <script src="..\assets\pages\widget\amchart\light.js"></script>
-    <script type="text/javascript" src="..\assets\js\SmoothScroll.js"></script>
-    <script src="..\assets\js\pcoded.min.js"></script>
-    <script src="..\assets\js\jquery.mCustomScrollbar.concat.min.js"></script>
-    <script src="..\assets\js\vartical-layout.min.js"></script>
-    <script type="text/javascript" src="..\assets\pages\dashboard\analytic-dashboard.min.js"></script>
-    <script type="text/javascript" src="..\assets\js\script.js"></script>
-<script async="" src="https://www.googletagmanager.com/gtag/js?id=UA-23581568-13"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'UA-23581568-13');
-</script>
-</body>
-
+        <script type="text/javascript" src="..\assets\bower_components\jquery\js\jquery.min.js"></script>
+        <script type="text/javascript" src="..\assets\bower_components\jquery-ui\js\jquery-ui.min.js"></script>
+        <script type="text/javascript" src="..\assets\bower_components\popper.js\js\popper.min.js"></script>
+        <script type="text/javascript" src="..\assets\bower_components\bootstrap\js\bootstrap.min.js"></script>
+        <script type="text/javascript" src="..\assets\bower_components\jquery-slimscroll\js\jquery.slimscroll.js"></script>
+        <script type="text/javascript" src="..\assets\bower_components\modernizr\js\modernizr.js"></script>
+        <script type="text/javascript" src="..\assets\bower_components\modernizr\js\css-scrollbars.js"></script>
+        <script type="text/javascript" src="..\assets\bower_components\chart.js\js\Chart.js"></script>
+        <script src="..\assets\pages\widget\amchart\amcharts.js"></script>
+        <script src="..\assets\pages\widget\amchart\serial.js"></script>
+        <script src="..\assets\pages\widget\amchart\light.js"></script>
+        <script type="text/javascript" src="..\assets\js\SmoothScroll.js"></script>
+        <script src="..\assets\js\pcoded.min.js"></script>
+        <script src="..\assets\js\jquery.mCustomScrollbar.concat.min.js"></script>
+        <script src="..\assets\js\vartical-layout.min.js"></script>
+        <script type="text/javascript" src="..\assets\pages\dashboard\analytic-dashboard.min.js"></script>
+        <script type="text/javascript" src="..\assets\js\script.js"></script>
+        <script async="" src="https://www.googletagmanager.com/gtag/js?id=UA-23581568-13"></script>
+        <script>
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'UA-23581568-13');
+        </script>
+    </body>
 </html>

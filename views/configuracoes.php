a60bf5305f3871e0ee80ce519062946a0c894a22
support4infra <suporte@4infra.com.br>
2023-05-03 09:41:02 -0300

    ajustes form layout das configurações

--- views/configuracoes.php
+++ views/configuracoes.php
@@ -20,34 +20,88 @@
         </div>
     </div>
 </div>
+
 <div class="card">
-    <div class="card-header">
-        <h5>Layout Menu</h5>
-    </div>
     <div class="card-block">
-        <div class="form-radio">
-            <form>
-                <div class="radio radio-inline theme-color">
-                    <label>
-                        <input type="radio" name="radio" checked="checked">
-                        <a href="#" class="navbar-theme" navbar-theme="themelight1">
-                            <span class="head"></span>
-                            <span class="cont"></span>
-                        </a>
-                        <i class="helper"></i>
-                        <input type="radio" name="radio" checked="checked">
-                    </label>
-                    <label>
-                        <input type="radio" name="radio" checked="checked">
-                        <a href="#" class="navbar-theme" navbar-theme="theme1">
-                            <span class="head"></span>
-                            <span class="cont"></span>
-                        </a>
-                        <i class="helper"></i>
-                        <input type="radio" name="radio" checked="checked">
-                    </label>
+        <div class="row">
+            <div class="col-sm-12 col-md-6 col-xl-4 m-b-30">
+                <h4 class="sub-title">Layout Menu</h4>
+                <div class="form-radio">
+                    <form>
+                        <div class="radio radio-inline">
+                            <label>
+                                <input type="radio" name="radio" value="theme1">
+                                <i class="helper"></i> <img src="../assets/images/theme1.png" alt="">
+                            </label>
+                        </div>
+                        <div class="radio radio-inline">
+                            <label>
+                                <input type="radio" name="radio" value="theme2">
+                                <i class="helper"></i> <img src="../assets/images/theme2.png" alt="">
+                            </label>
+                        </div>
+                        <button type="submit" class="btn btn-primary m-b-0">salvar</button>
+                    </form>
                 </div>
-            </form>
+            </div>
+            <div class="col-sm-12 col-md-6 col-xl-4 m-b-30">
+                <h4 class="sub-title">Layout Topo</h4>
+                <div class="form-radio">
+                    <form>
+                        <div class="radio radio-inline">
+                            <label>
+                                <input type="radio" name="radio" value="themeTopo1">
+                                <i class="helper"></i> <img src="../assets/images/themeTopo1.png" alt="">
+                            </label>
+                        </div>
+                        <div class="radio radio-inline">
+                            <label>
+                                <input type="radio" name="radio" value="themeTopo2">
+                                <i class="helper"></i> <img src="../assets/images/themeTopo2.png" alt="">
+                            </label>
+                        </div>
+                        <div class="radio radio-inline">
+                            <label>
+                                <input type="radio" name="radio" value="themeTopo3">
+                                <i class="helper"></i> <img src="../assets/images/themeTopo3.png" alt="">
+                            </label>
+                        </div>
+                        <div class="radio radio-inline">
+                            <label>
+                                <input type="radio" name="radio" value="themeTopo4">
+                                <i class="helper"></i> <img src="../assets/images/themeTopo4.png" alt="">
+                            </label>
+                        </div>
+                        <div class="radio radio-inline">
+                            <label>
+                                <input type="radio" name="radio" value="themeTopo5">
+                                <i class="helper"></i> <img src="../assets/images/themeTopo5.png" alt="">
+                            </label>
+                        </div>
+                        <div class="radio radio-inline">
+                            <label>
+                                <input type="radio" name="radio" value="themeTopo6">
+                                <i class="helper"></i> <img src="../assets/images/themeTopo6.png" alt="">
+                            </label>
+                        </div>
+                        <button type="submit" class="btn btn-primary m-b-0">salvar</button>
+                    </form>
+                </div>
+            </div>   
+            <div class="col-sm-12 col-md-6 col-xl-4 m-b-30">
+                <h4 class="sub-title">Layout Link Ativo</h4>
+                <div class="form-radio">
+                    <form>
+                        <div class="radio radio-inline">
+                            <label>
+                                <input type="radio" name="radio" value="theme1">
+                                <div class="theme-color"><a href="#" class="active-item-theme small" active-item-theme="theme1">&nbsp;</a><a href="#" class="active-item-theme small" active-item-theme="theme2">&nbsp;</a><a href="#" class="active-item-theme small" active-item-theme="theme3">&nbsp;</a><a href="#" class="active-item-theme small" active-item-theme="theme4">&nbsp;</a><a href="#" class="active-item-theme small" active-item-theme="theme5">&nbsp;</a><a href="#" class="active-item-theme small" active-item-theme="theme6">&nbsp;</a><a href="#" class="active-item-theme small" active-item-theme="theme7">&nbsp;</a><a href="#" class="active-item-theme small" active-item-theme="theme8">&nbsp;</a><a href="#" class="active-item-theme small" active-item-theme="theme9">&nbsp;</a><a href="#" class="active-item-theme small" active-item-theme="theme10">&nbsp;</a><a href="#" class="active-item-theme small" active-item-theme="theme11">&nbsp;</a><a href="#" class="active-item-theme small" active-item-theme="theme12">&nbsp;</a></div>
+                            </label>
+                        </div>
+                        <button type="submit" class="btn btn-primary m-b-0">salvar</button>
+                    </form>
+                </div>
+            </div>
         </div>
     </div>
 </div>

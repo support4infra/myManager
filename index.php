4e112a18ba8dbd23fc1e68a653d39b77b9625a3c
support4infra <suporte@4infra.com.br>
2023-05-05 11:52:05 -0300

    ajuste index

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 session_start();
 require 'config.php';
 
-define('BASE_URL', 'https://localhost/');
+define('BASE_URL', 'http://localhost/');
 
 spl_autoload_register(function ($class){
     if (strpos($class, 'Controller') > -1) {

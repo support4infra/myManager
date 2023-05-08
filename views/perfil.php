532b38da421a6a942b5624300c1d14d276356273
support4infra <suporte@4infra.com.br>
2023-05-08 10:21:43 -0300

    ajuste tabela Perfil

--- views/perfil.php
+++ views/perfil.php
@@ -1,126 +1,59 @@
-
 <div class="page-header">
-                                    <div class="row align-items-end">
-                                        <div class="col-lg-8">
-                                            <div class="page-header-title">
-                                                <div class="d-inline">
-                                                    <h4>HTML 5 Data Export</h4>
-                                                    <span>HTML5 buttons provides the local file saving features</span>
-                                                </div>
-                                            </div>
-                                        </div>
-                                        <div class="col-lg-4">
-                                            <div class="page-header-breadcrumb">
-                                                <ul class="breadcrumb-title">
-                                                    <li class="breadcrumb-item">
-                                                        <a href="index-1.htm"> <i class="feather icon-home"></i> </a>
-                                                    </li>
-                                                    <li class="breadcrumb-item"><a href="#!">Data Table Extensions</a>
-                                                    </li>
-                                                    <li class="breadcrumb-item"><a href="#!">Button</a>
-                                                    </li>
-                                                    <li class="breadcrumb-item"><a href="#!">HTML5 Data-Export</a>
-                                                    </li>
-                                                </ul>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="card">
-                                            <div class="card-header">
-                                                <h5>Edit With Button</h5>
-                                                <span>Click on buttons to perform actions</span>
-
-                                            </div>
-                                            <div class="card-block">
-                                                <div class="table-responsive">
-                                                    <table class="table table-striped table-bordered" id="example-2">
-                                                        <thead>
-                                                            <tr>
-                                                                <th>#</th>
-                                                                <th>First</th>
-                                                                <th>Last</th>
-                                                                <th>Nickname</th>
-                                                            <th class="tabledit-toolbar-column"></th></tr>
-                                                        </thead>
-                                                        <tbody>
-                                                            <tr>
-                                                                <th scope="row">1</th>
-                                                                <td class="tabledit-view-mode"><span class="tabledit-span">Mark
-                                                                    
-                                                                </span><input class="tabledit-input form-control input-sm" type="text" name="First Name" value="Mark
-                                                                    
-                                                                " style="display: none;" disabled=""></td>
-                                                                <td class="tabledit-view-mode"><span class="tabledit-span">Otto
-                                                                    
-                                                                </span><input class="tabledit-input form-control input-sm" type="text" name="Last Name" value="Otto
-                                                                    
-                                                                " style="display: none;" disabled=""></td>
-                                                                <td class="tabledit-view-mode"><span class="tabledit-span">@mdo</span>
-                                                                    <select class="tabledit-input form-control input-sm" name="Nickname" disabled="" style="display:none;">
-                                                <option value="1">@mdo</option>
-                                                <option value="2">@fat</option>
-                                                <option value="3">@twitter</option>
-                                            </select>
-                                                                </td>
-                                                            <td style="white-space: nowrap; width: 1%;"><div class="tabledit-toolbar btn-toolbar" style="text-align: left;">
-                                           <div class="btn-group btn-group-sm" style="float: none;"><button type="button" class="tabledit-edit-button btn btn-primary waves-effect waves-light" style="float: none;margin: 5px;"><span class="icofont icofont-ui-edit"></span></button><button type="button" class="tabledit-delete-button btn btn-danger waves-effect waves-light" style="float: none;margin: 5px;"><span class="icofont icofont-ui-delete"></span></button></div>
-                                           <button type="button" class="tabledit-save-button btn btn-sm btn-success" style="display: none; float: none;">Save</button>
-                                           <button type="button" class="tabledit-confirm-button btn btn-sm btn-danger" style="display: none; float: none;">Confirm</button>
-                                           <button type="button" class="tabledit-restore-button btn btn-sm btn-warning" style="display: none; float: none;">Restore</button>
-                                       </div></td></tr>
-                                                            <tr>
-                                                                <th scope="row">2</th>
-                                                                <td class="tabledit-view-mode"><span class="tabledit-span">Jacob
-                                                                    
-                                                                </span><input class="tabledit-input form-control input-sm" type="text" name="First Name" value="Jacob
-                                                                    
-                                                                " style="display: none;" disabled=""></td>
-                                                                <td class="tabledit-view-mode"><span class="tabledit-span">Thorntonkk
-                                                                    
-                                                                </span><input class="tabledit-input form-control input-sm" type="text" name="Last Name" value="Thorntonkk
-                                                                    
-                                                                " style="display: none;" disabled=""></td>
-                                                                <td class="tabledit-view-mode"><span class="tabledit-span">@mdo</span>
-                                                                    <select class="tabledit-input form-control input-sm" name="Nickname" disabled="" style="display:none;">
-                                                <option value="1">@mdo</option>
-                                                <option value="2">@fat</option>
-                                                <option value="3">@twitter</option>
-                                            </select>
-                                                                </td>
-                                                            <td style="white-space: nowrap; width: 1%;"><div class="tabledit-toolbar btn-toolbar" style="text-align: left;">
-                                           <div class="btn-group btn-group-sm" style="float: none;"><button type="button" class="tabledit-edit-button btn btn-primary waves-effect waves-light" style="float: none;margin: 5px;"><span class="icofont icofont-ui-edit"></span></button><button type="button" class="tabledit-delete-button btn btn-danger waves-effect waves-light" style="float: none;margin: 5px;"><span class="icofont icofont-ui-delete"></span></button></div>
-                                           <button type="button" class="tabledit-save-button btn btn-sm btn-success" style="display: none; float: none;">Save</button>
-                                           <button type="button" class="tabledit-confirm-button btn btn-sm btn-danger" style="display: none; float: none;">Confirm</button>
-                                           <button type="button" class="tabledit-restore-button btn btn-sm btn-warning" style="display: none; float: none;">Restore</button>
-                                       </div></td></tr>
-                                                            <tr>
-                                                                <th scope="row">3</th>
-                                                                <td class="tabledit-view-mode"><span class="tabledit-span">Larry
-                                                                    
-                                                                </span><input class="tabledit-input form-control input-sm" type="text" name="First Name" value="Larry
-                                                                    
-                                                                " style="display: none;" disabled=""></td>
-                                                                <td class="tabledit-view-mode"><span class="tabledit-span">the Bird
-                                                                    
-                                                                </span><input class="tabledit-input form-control input-sm" type="text" name="Last Name" value="the Bird
-                                                                    
-                                                                " style="display: none;" disabled=""></td>
-                                                                <td class="tabledit-view-mode"><span class="tabledit-span">@mdo</span>
-                                                                    <select class="tabledit-input form-control input-sm" name="Nickname" disabled="" style="display:none;">
-                                                <option value="1">@mdo</option>
-                                                <option value="2">@fat</option>
-                                                <option value="3">@twitter</option>
-                                            </select>
-                                                                </td>
-                                                            <td style="white-space: nowrap; width: 1%;"><div class="tabledit-toolbar btn-toolbar" style="text-align: left;">
-                                           <div class="btn-group btn-group-sm" style="float: none;"><button type="button" class="tabledit-edit-button btn btn-primary waves-effect waves-light" style="float: none;margin: 5px;"><span class="icofont icofont-ui-edit"></span></button><button type="button" class="tabledit-delete-button btn btn-danger waves-effect waves-light" style="float: none;margin: 5px;"><span class="icofont icofont-ui-delete"></span></button></div>
-                                           <button type="button" class="tabledit-save-button btn btn-sm btn-success" style="display: none; float: none;">Save</button>
-                                           <button type="button" class="tabledit-confirm-button btn btn-sm btn-danger" style="display: none; float: none;">Confirm</button>
-                                           <button type="button" class="tabledit-restore-button btn btn-sm btn-warning" style="display: none; float: none;">Restore</button>
-                                       </div></td></tr>
-                                                        </tbody>
-                                                    </table>
-                                                </div>
-                                            </div>
-                                        </div>
+    <div class="row align-items-end">
+        <div class="col-lg-8">
+            <div class="page-header-title">
+                <div class="d-inline">
+                    <h4>Perfil</h4>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-4">
+            <div class="page-header-breadcrumb">
+                <ul class="breadcrumb-title">
+                    <li class="breadcrumb-item">
+                        <a href="<?php echo BASE_URL;?>dashboard"> <i class="fa fa-dashboard"></i> </a>
+                    </li>
+                    <li class="breadcrumb-item">Administração
+                    </li>
+                    <li class="breadcrumb-item"><a href="<?php echo BASE_URL;?>perfil">Perfil</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="card">
+    <div class="card-block">
+        <div class="table-responsive">
+            <table class="table table-striped table-bordered" id="example-2">
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>NOME</th>
+                        <th>AÇÃO</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <th scope="row">1</th>
+                        <td class="tabledit-view-mode">
+                            <span class="tabledit-span">Super-Admin</span>
+                        </td>
+                        <td style="white-space: nowrap; width: 1%;">
+                            <div class="tabledit-toolbar btn-toolbar" style="text-align: left;">
+                                <div class="btn-group btn-group-sm" style="float: none;">
+                                    <button type="button" class="tabledit-edit-button btn btn-primary waves-effect waves-light" style="float: none;margin: 5px;">
+                                        <span class="icofont icofont-ui-edit"></span>
+                                    </button>
+                                    <button type="button" class="tabledit-delete-button btn btn-danger waves-effect waves-light" style="float: none;margin: 5px;">
+                                        <span class="icofont icofont-ui-delete"></span>
+                                    </button>
+                                </div>    
+                            </div>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+</div>

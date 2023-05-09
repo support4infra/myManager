c44887acd8a5449ab685d16eb89c3165231c901b
support4infra <suporte@4infra.com.br>
2023-05-09 17:54:41 -0300

    View relatorio sem select da bancada

---
+++ views/relatorioBancada.php
@@ -0,0 +1,53 @@
+<div class="card">
+    <div class="card-block">
+        <div class="table-responsive">
+            <table class="table table-striped table-bordered" id="example-2">
+                <thead>
+                    <tr>
+                        <th>TICKET</th>
+                        <th>CLIENTE</th>
+                        <th>ORIGEM DA AQUISIÇÃO</th>
+                        <th>DATA DE ABERTURA</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <th scope="row">1</th>
+                        <td class="tabledit-view-mode">
+                            <span class="tabledit-span">TESTE</span>
+                        </td>
+                        <td style="white-space: nowrap; width: 1%;">
+                            
+                        </td>
+                        <td style="white-space: nowrap; width: 1%;">
+                            
+                        </td>
+                        <td style="white-space: nowrap; width: 1%;">
+                            
+                        </td>
+
+                        <td style="white-space: nowrap; width: 1%;">
+                            
+                        </td>
+                        <td style="white-space: nowrap; width: 1%;">
+                            <div class="tabledit-toolbar btn-toolbar" style="text-align: left;">
+                                <div class="btn-group btn-group-sm" style="float: none;">
+                                    <button type="button" class="tabledit-edit-button btn btn-primary waves-effect waves-light" style="float: none;margin: 5px;">
+                                        <span class="icofont icofont-ui-edit"></span>
+                                    </button>
+                                    <button type="button" class="tabledit-warning-button btn btn-warning waves-effect waves-light" style="float: none;margin: 5px;">
+                                        <span class="fa fa-eye"></span>
+                                    </button>
+                                    <button type="button" class="tabledit-delete-button btn btn-danger waves-effect waves-light" style="float: none;margin: 5px;">
+                                        <span class="icofont icofont-ui-delete"></span>
+                                    </button>
+                                </div>    
+                            </div>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+</div>
+
